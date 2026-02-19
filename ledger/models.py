e9fb8bb718ac70c89ceb91d514bcae810c14f3ef
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-02-19 22:42:21 +0800

    added models for migrations

--- ledger/models.py
+++ ledger/models.py
@@ -1,1 +1,31 @@
 from django.db import models
+from django.urls import reverse
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[str(self.id)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+    def __str__(self):
+        return self.quantity
