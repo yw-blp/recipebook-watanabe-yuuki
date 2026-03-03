c84ce76bf0ad978d3dd6bececd539940fe8329c7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-03 18:31:31 +0800

    added admin page for the database

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display = ('name',)
+    search_fields = ("name", )
+    inlines = [RecipeIngredientInLine, ]
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    list_display = ('name',)
+    search_fields = ("name", )
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
