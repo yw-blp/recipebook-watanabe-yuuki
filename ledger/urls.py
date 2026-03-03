a9ba70d2375cb735ba0219c87e20a6f4b58e9115
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-03 16:35:55 +0800

    adjusted name of path recipes/list to fit the adjusted reverse argument from the Recipe class

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-name'),
 ]
 
 app_name = 'ledger'
