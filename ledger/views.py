660ea4f6b620e499b2d7515f18a370b6334e1d26
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-03 16:37:04 +0800

    adjusted code to fit with pep8 standards

--- ledger/views.py
+++ ledger/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Recipe
 
+
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
+
 
 class RecipeDetailView(DetailView):
     model = Recipe
