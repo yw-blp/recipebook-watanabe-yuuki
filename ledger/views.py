2ae7721ef6ecfd50c53485b67b759e279923ed57
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-04 20:16:20 +0800

    added LoginRequiredMixin in RecipeDetailView to prohibit entry when not logged in

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 
@@ -8,6 +9,6 @@ class RecipeListView(ListView):
     template_name = 'recipe_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
