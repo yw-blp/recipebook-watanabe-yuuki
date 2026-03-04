9c870f280f05548630645cdb40ef13f6c6cfcd5d
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-04 20:12:07 +0800

    added Profile model as well as adding more fields to Recipe model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(256,
+                               'Field must contain more than 255 characters')
+        ]
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -11,12 +27,19 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='profiles'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('ledger:recipe-name', args=[str(self.id)])
+        return reverse('ledger:recipe-detail', args=[str(self.id)])
 
 
 class RecipeIngredient(models.Model):
