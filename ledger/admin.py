fd5d8d8513daa64ac286411841af16041a424555
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-04 20:13:22 +0800

    added UserAdmin and modified RecipeAdmin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,19 +1,37 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Profile, Ingredient, Recipe, RecipeIngredient
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    search_fields = ('name', )
+    inlines = [ProfileInLine, ]
+
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    list_display = ('name',)
-    search_fields = ("name", )
+    list_display = ('name', 'author', 'created_on', 'updated_on')
+    search_fields = ('name', )
     inlines = [RecipeIngredientInLine, ]
+
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
-    list_display = ('name',)
-    search_fields = ("name", )
+    list_display = ('name', )
+    search_fields = ('name', )
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
