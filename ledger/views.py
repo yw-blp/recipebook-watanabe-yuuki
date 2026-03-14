e6a87a30f5b1b082d5a707bcf22ba74842db78c0
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-14 18:21:53 +0800

    added create views for forms related to creating recipes by users

--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Recipe
+from django.urls import reverse_lazy
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 
 class RecipeListView(ListView):
@@ -12,3 +15,33 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "recipe_add.html"
+    success_url = "/recipes/list"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = "recipe_update_add_image.html"
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe-detail',
+                            kwargs={'pk': self.object.recipe.pk})
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe"] = Recipe.objects.get(pk=self.kwargs['pk'])
+        return context
