becd66fba02fda5ee56cc545cc5f481a54dcf63d
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-14 18:23:23 +0800

    added the new form templates to create new recipes and connected it to their respective create views

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import (RecipeListView, RecipeCreateView, RecipeDetailView,
+                    RecipeImageCreateView)
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-add'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipe/<int:pk>/add_image', RecipeImageCreateView.as_view(),
+         name='recipe-update-add-image'),
 ]
 
 app_name = 'ledger'
