7d190890a771826791dfb4e0d9fb9aa4c2d0b695
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-14 18:15:48 +0800

    configured to work with media files

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
